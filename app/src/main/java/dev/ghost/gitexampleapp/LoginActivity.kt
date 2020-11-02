1cae3a0f1bcdebcd34ffc693dae0df0da3c7ada8
Bulka <polly4656@gmail.com>
2020-11-02 08:59:02 +0300

    Добавлена страница входа.

---
+++ app/src/main/java/dev/ghost/gitexampleapp/LoginActivity.kt
@@ -0,0 +1,17 @@
+package dev.ghost.gitexampleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        buttonLogin.setOnClickListener {
+            Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
